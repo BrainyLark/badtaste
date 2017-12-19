9f368904be07999126592665572d423019d2d9a2
Erdenebileg <erdenebileg.byambadorj@gmail.com>
2017-12-20 03:48:26 +0800

    90% done

--- app/src/main/java/com/archer/badtaste/ListAdapter.java
+++ app/src/main/java/com/archer/badtaste/ListAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.RatingBar;
@@ -23,10 +24,12 @@ public class ListAdapter extends BaseAdapter {
     private List<Movie> movies;
     Context context;
     LayoutInflater layoutInflater;
+    MainActivity mainActivity;
 
     public ListAdapter(Context context, List<Movie> movies) {
         this.context = context;
         this.movies = movies;
+        mainActivity = (MainActivity) context;
     }
 
     @Override
@@ -49,17 +52,25 @@ public class ListAdapter extends BaseAdapter {
         layoutInflater = LayoutInflater.from(this.context);
 
         final Movie movie = movies.get(i);
+        final int position = i;
         view = layoutInflater.inflate(R.layout.movie_row_item, null);
         Picasso.with(context).load("https://image.tmdb.org/t/p/w154" + movie.getPoster()).into((ImageView)view.findViewById(R.id.posterView));
         ((TextView)view.findViewById(R.id.idView)).setText(String.valueOf(movie.getId()));
         ((TextView)view.findViewById(R.id.titleView)).setText(movie.getTitle());
         ((TextView)view.findViewById(R.id.genresView)).setText(movie.getGenres());
+        ((RatingBar)view.findViewById(R.id.ratingBar)).setRating((float) movie.getPredicted());
         ((RatingBar)view.findViewById(R.id.ratingBar)).setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
-                Toast.makeText(context, String.valueOf(v), Toast.LENGTH_LONG).show();
+                mainActivity.setCurrentItem(position);
+                Toast.makeText(context, "Rating: " + v + " MOVIEID: " + movie.getId(), Toast.LENGTH_SHORT).show();
+                mainActivity.contribute(v, movie.getId());
             }
         });
         return view;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
     }
 }
--- app/src/main/java/com/archer/badtaste/MainActivity.java
+++ app/src/main/java/com/archer/badtaste/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -14,7 +15,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,8 +31,9 @@ public class MainActivity extends AppCompatActivity {
     private ProgressDialog pd;
     private int userid;
     private String username;
-    ListAdapter listAdapter;
-    ListView listView;
+    public ListAdapter listAdapter;
+    private ListView listView;
+    private int currentItem;
 
     public void init() {
         pd = new ProgressDialog(MainActivity.this, R.style.myTheme);
@@ -42,6 +50,19 @@ public class MainActivity extends AppCompatActivity {
     public void recommend() {
         RecommendTask recommendTask = new RecommendTask();
         recommendTask.execute(userid);
+    }
+
+    public void setCurrentItem(int item) {
+        currentItem = item;
+    }
+
+    public void contribute(float rating, long itemId) {
+        Bundle bundle = new Bundle();
+        bundle.putLong("userId", (long)userid);
+        bundle.putDouble("rating", (double)rating);
+        bundle.putLong("itemId", itemId);
+        ContributeTask contributeTask = new ContributeTask();
+        contributeTask.execute(bundle);
     }
 
     @Override
@@ -94,4 +115,63 @@ public class MainActivity extends AppCompatActivity {
             pd.dismiss();
         }
     }
+
+    class ContributeTask extends AsyncTask<Bundle, Void, Boolean> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd.show();
+        }
+
+        @Override
+        protected Boolean doInBackground(Bundle... bundles) {
+            JSONObject data = new JSONObject();
+            HttpURLConnection urlConn;
+            try {
+                data.put("userId", bundles[0].getLong("userId"));
+                data.put("rating", bundles[0].getDouble("rating"));
+                data.put("itemId", bundles[0].getLong("itemId"));
+                String json = data.toString();
+
+                URL mUrl = new URL("http://192.168.1.7:8080/contribute");
+                urlConn = (HttpURLConnection) mUrl.openConnection();
+                urlConn.addRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                urlConn.setDoOutput(true);
+                urlConn.setDoInput(true);
+                urlConn.setRequestMethod("POST");
+                OutputStream os = urlConn.getOutputStream();
+                os.write(json.getBytes());
+                os.close();
+
+                InputStream in = new BufferedInputStream(urlConn.getInputStream());
+
+                String result = IOUtils.toString(in, "UTF-8");
+                JSONObject jsonObject = new JSONObject(result);
+                return jsonObject.getBoolean("success");
+
+            } catch (JSONException e) {
+
+            } catch (ProtocolException e) {
+            } catch (MalformedURLException e) {
+            } catch (IOException e) {
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            super.onPostExecute(success);
+            if(success) {
+                listAdapter.getMovies().remove(currentItem);
+                listAdapter.notifyDataSetChanged();
+                pd.dismiss();
+                Toast.makeText(MainActivity.this, "Таны үнэлгээ амжилттай хадгалагдлаа! Хувь нэмрээ оруулсан танд баярлалаа!", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Уучлаарай, таны үнэлгээ ороход алдаа гарлаа!", Toast.LENGTH_LONG).show();
+                pd.dismiss();
+            }
+        }
+    }
+
 }
